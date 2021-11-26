d338d3ab180c3916f2fc6504db4dc4c1e9fb9aa5
ИСП-43 <-43@LAB24-08>
2021-11-26 17:50:07 +0300

    Первый вариант кода

--- Interface/GeometricProgression.cs
+++ Interface/GeometricProgression.cs
@@ -7,15 +7,17 @@ namespace Interface
     class GeometricProgression : IIndexableSeries
     {
         double IIndexable.this[int index] => throw new NotImplementedException();
+
         int start = 2;
         int val = 2;
         int val1 = 4;
+
         double ISeries.GetCurrent()
         {
             val  = val * val1;
             return val;
         }
-
+         
         bool ISeries.MoveNext()
         {
             return true;
